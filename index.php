d86a4a9effe7f1b5581a7ecf49a64b48d1ce8273
lechaps <33101610+lechaps@users.noreply.github.com>
2018-07-27 15:26:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
+/*************** F I R S T   S O L U T I O N ******************/
 function fizzbuzz($n) {
-    $flag=true;
-    if ($n % 3 == 0) {
-      print("Fizz");
-      $flag=false;
-    }
-    if ($num % 5 == 0) {
-      print("Buzz");
-      $flag=false;
-    }
-    if ($flag) print($n);
+    $s="";
+    if ($n % 3 == 0) $s.="Fizz";
+    if ($n % 5 == 0) $s.="Buzz";
+    if (empty($s) $s=$n;
 }
 for ($i = 0; $i <=100; $i++)
 {
-    fizzbuzz($i);
+    print (fizzbuzz($i));
 }
+/*************** S E C O N D   S O L U T I O N ******************/
+<?php
+for($i = 1; $i <= 100 and print(($i % 15 ? $i % 5 ? $i % 3 ? $i : 'Fizz' : 'Buzz' : 'FizzBuzz') . "\n"); ++$i);
+?>
+
+?>
+
